3d40abaa5aeb5b5a4f940a0064db15dbb4e59e6d
Artur Ivanov <artur@work-macbook.local>
2020-09-11 12:33:09 +0200

    initial commit

---
+++ assets/upvoits.js
@@ -0,0 +1,12 @@
+export const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+    },
+    'learn-node': {
+        upvotes: 0,
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+    },
+}
+
---
+++ src/build/precache-manifest.f515f5d83e7d209692d345a7c48eb583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb757de5cb904cc5fac59b08d7290e61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c19adac28f0e291ec796",
+    "url": "/static/css/main.4141219e.chunk.css"
+  },
+  {
+    "revision": "13cefec9fa6314f6244a",
+    "url": "/static/js/2.cd50cf10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd50cf10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19adac28f0e291ec796",
+    "url": "/static/js/main.2104c705.chunk.js"
+  },
+  {
+    "revision": "9e2ae113827a28fdb7f5",
+    "url": "/static/js/runtime-main.1ac87278.js"
+  }
+]);
